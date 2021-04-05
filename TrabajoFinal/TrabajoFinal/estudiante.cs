bcc6a8f89f1552a283a37a942272114d750b6133
Nycolperdomo <79879263+Nycolperdomo@users.noreply.github.com>
2021-04-04 23:04:45 -0500

    Add files via upload

---
+++ TrabajoFinal/TrabajoFinal/estudiante.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabajoFinal
+{
+    class estudiante
+    {
+        public int Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public double Nota1 { get; set; }
+        public double Nota2 { get; set; }
+        public double Nota3 { get; set; }
+        public double NotaFin { get; set; }
+    }
+}
